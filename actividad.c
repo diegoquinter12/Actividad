4b309092c2df5e5341d36c4f2723d61618794e68
DIEGO <diegoquinter3@gmail.com>
2026-04-30 17:31:42 -0500

    first commit

---
+++ actividad.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+ int a, b;
+
+  printf("Ingresar el primer valor: ");
+    scanf("%d", &a);
+
+    printf("Ingresar el segundo valor: ");
+    scanf("%d", &b);
+
+    }
